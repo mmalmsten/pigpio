a4b614c286d4a3087f0e53ad1a6a138458009403
madeleine_malmsten <madeleine_malmsten@mckinsey.com>
2020-10-25 14:42:50 +0100

    Library structure

--- Makefile
+++ Makefile
@@ -1,1 +1,7 @@
+PROJECT = Pigpio
+PROJECT_DESCRIPTION = Using Erlang for GPIO on a Raspberry Pi
+PROJECT_VERSION = 0.1.0
+
+LOCAL_DEPS = inets
+
 include erlang.mk
--- src/pigpio.erl
+++ src/pigpio.erl
@@ -1,75 +1,107 @@
 -module(pigpio).
+
 -author(skvamme).
--export([start/0,init/0,loop/1]).
--define(PIGPIO_IP,"192.168.0.20").
--define(PIGPIO_PORT,8888).
--define(UINT,32/little).
 
-start() -> Pid = spawn_link(?MODULE,init,[]), {ok,Pid}.
+-author(mmalmsten).
 
-init() ->
-        io:format("New process: ~p~n", [?MODULE]),
-        inets:start(),
-        {ok,Socket} = gen_tcp:connect(?PIGPIO_IP, ?PIGPIO_PORT, [binary,{packet, 0}]),
-        ok = gen_tcp:send(Socket,c(setmode,25,0)),
-        ok = gen_tcp:send(Socket,c(getmode,25)),
-        callback:make(self(),33554432), % Start a callback to monitor pin 25 on a dedicated socket
-        %timer:send_interval(10*1000, {timer}),
-        loop(Socket).
+-behaviour(gen_server).
 
-loop(Socket) ->
-        receive
-                % {timer} -> ok = gen_tcp:send(Socket,c(br1)),
-                %         ?MODULE:loop(Socket);
-                {tcp,Socket,Data} ->
-                        parse(Socket,Data),
-                        ?MODULE:loop(Socket);
-                {kwh,Sec} when Sec > 0 -> 
-                        W = 3600000000 div Sec,
-                        io:format("W: ~p~n",[W]),
-                        ?MODULE:loop(Socket);
-                Any -> io:format("~p got unknown msg: ~p~n",[?MODULE, Any]),
-                        ?MODULE:loop(Socket)
-        end.
+-export([connect/3, read/1, write/2]).
 
-%******************************************************************************
+-export([handle_call/3, handle_cast/2, handle_info/2,
+	 init/1, start_link/3]).
+
+%%
+%% pigpio commands
+%%
+
+connect(Ip, Port, Gpio) ->
+    {ok, Pid} = start_link(Ip, Port, Gpio),
+    register(list_to_atom(integer_to_list(Gpio)), Pid),
+    {ok, Pid}.
+
+read(Pid) -> gen_server:call(Pid, read).
+
+write(Pid, Msg) -> gen_server:call(Pid, {write, Msg}).
+
+%%
+%% Handle gen_server and tcp connection
+%%
+
+start_link(Ip, Port, Gpio) ->
+    {ok, Pid} = gen_server:start_link(?MODULE,
+				      {Ip, Port, Gpio}, []),
+    {ok, Pid}.
+
+init(_) -> {ok, []}.
+
+% init({_Ip, _Port, _Gpio}) ->
+    % application:start(inets),
+    % TODO: calculate magic number from "Gpio" here
+    % Bits = 33554432,
+    % {ok, Socket} = gen_tcp:connect(Ip, Port,
+
+        % 			   [binary, {packet, 0}]),
+
+    % ok = gen_tcp:send(Socket,
+        % 	      <<0:32, Gpio:32, 0:32, 0:32>>),
+
+    % ok = gen_tcp:send(Socket,
+        % 	      <<1:32, Gpio:32, 0:32, 0:32>>),
+
+    % ok = gen_tcp:send(Socket, <<99:32, 0:32, 0:32, 0:32>>),
+    % ok = gen_tcp:send(Socket,
+        % 	      <<19:32, 0:32, Bits:32, 0:32>>),
+
+    % {ok, [Socket]}.
+
+handle_call(read, _, State) ->
+    {reply, round(rand:uniform()) == 1,
+     State}; % Simulator for now
+handle_call({write, Msg}, _, State) ->
+    {reply, Msg, State};
+handle_call(terminate, _, State) ->
+    {stop, normal, ok, State}.
+
+handle_cast(Msg, State) ->
+    io:format("Unexpected message: ~p~n", [Msg]),
+    {noreply, State}.
+
+handle_info(Msg, State) ->
+    io:format("Unexpected message: ~p~n", [Msg]),
+    {noreply, State}.
+
+%%
 % pigpio response
-%******************************************************************************
-parse(_Socket,<<>>) -> ok;
-parse(Socket,<<0:?UINT,P1:?UINT,P2:?UINT,0:?UINT,Rest/binary>>) ->
-         io:format("setmode pin ~p mode ~p~n",[P1,P2]),
-         parse(Socket,Rest);
-parse(Socket,<<1:?UINT,P1:?UINT,_P2:?UINT,P3:?UINT,Rest/binary>>) ->
-         io:format("getmode pin ~p mode ~p~n",[P1,P3]),
-         parse(Socket,Rest);
-parse(Socket,<<2:?UINT,P1:?UINT,P2:?UINT,0:?UINT,Rest/binary>>) ->
-         io:format("setpullupdown pin ~p pud ~p~n",[P1,P2]),
-         parse(Socket,Rest);
-parse(Socket,<<3:?UINT,_P1:?UINT,_P2:?UINT,P3:?UINT,Rest/binary>>) ->
-         io:format("read ~p~n",[P3]),
-         parse(Socket,Rest);
-parse(Socket,<<4:?UINT,P1:?UINT,P2:?UINT,0:?UINT,Rest/binary>>) ->
-         io:format("write pin ~p level ~p~n",[P1,P2]),
-         parse(Socket,Rest);
-parse(Socket,<<10:?UINT,_P1:?UINT,_P2:?UINT,P3:?UINT,Rest/binary>>) ->
-         io:format("readbits 0-31 ~.2B~n",[P3]),
-         parse(Socket,Rest);
-parse(Socket,<<17:?UINT,_P1:?UINT,_P2:?UINT,P3:?UINT,Rest/binary>>) ->
-         io:format("hver ~p~n",[P3]),
-         parse(Socket,Rest);
-parse(_Socket,Response) -> io:format("Error: ~w~n",[Response]).
+%%
 
-%******************************************************************************
-% pigpio commands
-%******************************************************************************
-%c(hver) -> <<17:?UINT,0:?UINT,0:?UINT,0:?UINT>>;
-%c(br1) -> <<10:?UINT,0:?UINT,0:?UINT,0:?UINT>>;
-c(read,Gpio) -> <<3:?UINT,Gpio:?UINT,0:?UINT,0:?UINT>>;
-c(getmode,Gpio) -> <<1:?UINT,Gpio:?UINT,0:?UINT,0:?UINT>>.
-c(setmode,Gpio,Mode) -> <<0:?UINT,Gpio:?UINT,Mode:?UINT,0:?UINT>>; % Input = 0, Output = 1
-c(write,Gpio,Level) -> <<4:?UINT,Gpio:?UINT,Level:?UINT,0:?UINT>>;
-c(setpullupdown,Gpio,Pud) -> <<2:?UINT,Gpio:?UINT,Pud:?UINT,0:?UINT>>. % Off = 0, Down = 1, Up = 2
-
-
-
+% parse(_Socket, <<>>) -> ok;
+% parse(Socket,
+%       <<0:32, P1:32, P2:32, 0:32, Rest/binary>>) ->
+%     io:format("setmode pin ~p mode ~p~n", [P1, P2]),
+%     parse(Socket, Rest);
+% parse(Socket,
+%       <<1:32, P1:32, _P2:32, P3:32, Rest/binary>>) ->
+%     io:format("getmode pin ~p mode ~p~n", [P1, P3]),
+%     parse(Socket, Rest);
+% parse(Socket,
+%       <<2:32, P1:32, P2:32, 0:32, Rest/binary>>) ->
+%     io:format("setpullupdown pin ~p pud ~p~n", [P1, P2]),
+%     parse(Socket, Rest);
+% parse(Socket,
+%       <<3:32, _P1:32, _P2:32, P3:32, Rest/binary>>) ->
+%     io:format("read ~p~n", [P3]), parse(Socket, Rest);
+% parse(Socket,
+%       <<4:32, P1:32, P2:32, 0:32, Rest/binary>>) ->
+%     io:format("write pin ~p level ~p~n", [P1, P2]),
+%     parse(Socket, Rest);
+% parse(Socket,
+%       <<10:32, _P1:32, _P2:32, P3:32, Rest/binary>>) ->
+%     io:format("readbits 0-31 ~.2B~n", [P3]),
+%     parse(Socket, Rest);
+% parse(Socket,
+%       <<17:32, _P1:32, _P2:32, P3:32, Rest/binary>>) ->
+%     io:format("hver ~p~n", [P3]), parse(Socket, Rest);
+% parse(_Socket, Response) ->
+%     io:format("Error: ~w~n", [Response]).
 
